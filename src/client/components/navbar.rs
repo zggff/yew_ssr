d8e3ea43117cb27c4ed674dfef72d5d047070062
Max Giga <gigamaximwachau@gmail.com>
2022-11-29 18:03:23 +0300

    switch to axum

--- src/client/components/navbar.rs
+++ src/client/components/navbar.rs
@@ -4,7 +4,7 @@ use yew_router::prelude::*;
 
 #[function_component(Navbar)]
 pub fn navbar() -> Html {
-    return html!(
+    html!(
         <div class="bg-slate-200 flex justify-start px-8 py-2 gap-8">
             <Link<Route> to={Route::Home} classes="btn-blue">{"home"}</Link<Route>>
             <Link<Route> to={Route::Secure} classes="btn-blue">{"secure"}</Link<Route>>
--- src/server/server.rs
+++ src/server/server.rs
@@ -1,27 +1,46 @@
-use actix_cors::Cors;
-use actix_web::HttpRequest;
-use actix_web::{middleware, web, App, HttpResponse, HttpServer, Responder};
+use std::convert::Infallible;
 use std::error::Error;
-use std::fs;
+
+use axum::body::{Body, StreamBody};
+use axum::extract::State;
+use axum::handler::Handler;
+use axum::http::{Request, StatusCode};
+use axum::response::IntoResponse;
+use axum::routing::get_service;
+use axum::Router;
+use futures::stream::{self, StreamExt};
+use tower_http::services::ServeDir;
+
 mod cache;
 
-#[actix_web::get("{tail}*")]
-async fn render_yew_app(req: HttpRequest) -> impl Responder {
-    log::debug!("{:?}", req);
-    let index_html_fs = fs::read_to_string("./dist/index.html").unwrap();
-    let props = client::ServerAppProps {
-        url: req.uri().to_string().into(),
-    };
-    let content = yew::ServerRenderer::<client::ServerApp>::with_props(move || props)
-        .render()
-        .await;
-
-    HttpResponse::Ok()
-        .content_type("text/html; charset=utf-8")
-        .body(index_html_fs.replace("<body>", &format!("<body>{}", content)))
+#[derive(Clone)]
+struct YewRendererState {
+    index_html_before: String,
+    index_html_after: String,
 }
 
-#[actix_web::main]
+async fn render_yew_app(
+    State(state): State<YewRendererState>,
+    url: Request<Body>,
+) -> impl IntoResponse {
+    let renderer =
+        yew::ServerRenderer::<client::ServerApp>::with_props(move || client::ServerAppProps {
+            url: url.uri().to_string().into(),
+        });
+
+    StreamBody::new(
+        stream::once(async move { state.index_html_before })
+            .chain(renderer.render_stream())
+            .chain(stream::once(async move { state.index_html_after }))
+            .map(Result::<_, Infallible>::Ok),
+    )
+}
+
+async fn handle_error(_err: std::io::Error) -> impl IntoResponse {
+    (StatusCode::INTERNAL_SERVER_ERROR, "Something went wrong...")
+}
+
+#[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     dotenv::dotenv().ok();
     env_logger::init();
@@ -30,23 +49,37 @@ async fn main() -> Result<(), Box<dyn Error>> {
         Ok(port) => port.parse().unwrap_or(3000),
         _ => 3000,
     };
+    let addr = std::net::SocketAddr::from(([127, 0, 0, 1], port));
 
-    HttpServer::new(move || {
-        let cors = Cors::permissive();
-        App::new()
-            .wrap(middleware::Logger::default())
-            .wrap(middleware::Compress::default())
-            .service(
-                web::scope("/dist")
-                    .wrap(cache::CacheInterceptor::new(30))
-                    .service(actix_files::Files::new("", "./dist").use_last_modified(true)),
-            )
-            .service(render_yew_app)
-            .wrap(cors)
-    })
-    .bind(("0.0.0.0", port))?
-    .run()
-    .await?;
+    let index_html_s = tokio::fs::read_to_string("./dist/index.html")
+        .await
+        .expect("failed to read index.html");
+    let (index_html_before, index_html_after) = index_html_s.split_once("<body>").unwrap();
+    let mut index_html_before = index_html_before.to_owned();
+    index_html_before.push_str("<body>");
+
+    let index_html_after = index_html_after.to_owned();
+    let state = YewRendererState {
+        index_html_before,
+        index_html_after,
+    };
+
+    let renderer = render_yew_app.with_state(state);
+
+    let serve_dir = ServeDir::new("./dist");
+    let serve_dir = get_service(serve_dir)
+        .handle_error(handle_error)
+        .layer(cache::Cache::new(1));
+
+    let app = Router::new()
+        .nest_service("/dist", serve_dir)
+        .fallback_service(renderer);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 
     Ok(())
 }
+
